92d62ab3929232e194b97d475fd46b558cd1bb8a
Vitaly Tarasov <89269643535@mail.ru>
2024-06-18 08:48:37 +0300

    run test

--- Makefile
+++ Makefile
@@ -6,3 +6,6 @@ test:
 
 test-coverage:
 	XDEBUG_MODE=coverage vendor/bin/phpunit --coverage-clover clover.xml tests
+
+run:
+	gendiff src/file1.json src/file2.json
--- src/genDiff.php
+++ src/genDiff.php
@@ -2,15 +2,6 @@
 
 namespace Hexlet\Code;
 
-$autoloadPath1 = __DIR__ . '/../../../autoload.php';
-$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
-
-if (file_exists($autoloadPath1)) {
-    require_once $autoloadPath1;
-} else {
-    require_once $autoloadPath2;
-}
-
 function genDiff($file1, $file2): string
 {
     $file1 = file_get_contents($file1); //получаем содержимое файла из полученного пути
